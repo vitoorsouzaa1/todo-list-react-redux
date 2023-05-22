91b3ebb73978cc7b0097c1bee28767f9409afebf
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2023-05-22 11:56:25 -0300

    chore: clear default lines and add TitleComponent to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { TitleComponent } from './components/title-component/title.component'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <TitleComponent>TODO LIST</TitleComponent>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
