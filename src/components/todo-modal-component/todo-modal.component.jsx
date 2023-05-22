b6c837b7430f99e33066cdaca7a7b0cbbfc8b143
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2023-05-22 17:55:54 -0300

    feat: add TodoModalComponent and it's styles

---
+++ src/components/todo-modal-component/todo-modal.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Styles
+import './todo-modal.styles.scss'
+
+export const TodoModalComponent = () => {
+  return (
+    <div>
+      <h1>Modal</h1>
+    </div>
+  )
+}
