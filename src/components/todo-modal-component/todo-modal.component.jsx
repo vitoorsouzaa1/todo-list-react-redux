aec59f9522ecc3d2122c909f9b866e52dd6130b0
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2023-05-22 17:58:36 -0300

    feat: add logic for ModalComponent show when the state changes, add styles and started to use reducer and slicer

--- src/components/todo-modal-component/todo-modal.component.jsx
+++ src/components/todo-modal-component/todo-modal.component.jsx
@@ -1,12 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdOutlineClose } from 'react-icons/md'
+import { useDispatch } from 'react-redux'
+
+// Components
+import { ButtonComponent } from '../button-component/button.component'
+
+// Reducers & Slicers
+import { addTodo } from '../../redux/slices/todoSlice'
 
 // Styles
 import './todo-modal.styles.scss'
 
-export const TodoModalComponent = () => {
+export const TodoModalComponent = ({ modalOpen, setModalOpen }) => {
+  const [title, setTitle] = useState('')
+  const [status, setStatus] = useState('incomplete')
+
+  const dispatch = useDispatch()
+
+  const handleTaskSubmit = (e) => {
+    e.preventDefault()
+
+    if (title && status) {
+      dispatch(addTodo)
+    }
+  }
+
   return (
-    <div>
-      <h1>Modal</h1>
-    </div>
+    modalOpen && (
+      <div className='modal-wrapper'>
+        <div className='modal-container'>
+          <div
+            className='modal-closebtn'
+            onClick={() => setModalOpen(false)}
+            onKeyDown={() => setModalOpen(false)}
+          >
+            <MdOutlineClose />
+          </div>
+          <form className='form-container' onSubmit={(e) => handleTaskSubmit()}>
+            <h1 className='form-title'>Add Task</h1>
+            <label htmlFor='title'>
+              Title
+              <input
+                type='text'
+                id='title'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </label>
+            <label htmlFor='status'>
+              Status
+              <select
+                type='text'
+                id='status'
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                <option value='incomplete'>Incomplete</option>
+                <option value='complete'>Complete</option>
+              </select>
+            </label>
+            <div className='button-container'>
+              <ButtonComponent type='submit' variant='primary'>
+                Add Task
+              </ButtonComponent>
+              <ButtonComponent
+                type='button'
+                variant='secondary'
+                onClick={() => setModalOpen(false)}
+                onKeyDown={() => setModalOpen(false)}
+              >
+                Cancel
+              </ButtonComponent>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
   )
 }
