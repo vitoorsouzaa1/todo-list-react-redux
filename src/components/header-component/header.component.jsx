de061b46de99d13eff77c2ef3c69560fead76ed7
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2023-05-22 17:57:31 -0300

    feat: add logic for ModalComponent show when the state changes

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Styles
 import './header.styles.scss'
@@ -8,15 +8,19 @@ import { ButtonComponent, Selector } from '../button-component/button.component'
 import { TodoModalComponent } from '../todo-modal-component/todo-modal.component'
 
 export const AppHeaderComponent = () => {
+  const [modalOpen, setModalOpen] = useState(false)
+
   return (
     <div className='app-header'>
-      <ButtonComponent variant='primary'>Click Me</ButtonComponent>
+      <ButtonComponent variant='primary' onClick={() => setModalOpen(true)}>
+        Add Task
+      </ButtonComponent>
       <Selector id='status'>
         <option value='all'>All</option>
         <option value='incomplete'>Incomplete</option>
         <option value='complete'>Complete</option>
       </Selector>
-      <TodoModalComponent />
+      <TodoModalComponent modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   )
 }
