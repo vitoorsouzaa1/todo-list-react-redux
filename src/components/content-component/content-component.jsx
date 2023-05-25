ed1a31264f31626b5c7b8c7b7b6d5df96effee5c
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2023-05-25 14:20:55 -0300

    feat: add ContentComponent to show TodoItems

--- src/components/content-component/content-component.jsx
+++ src/components/content-component/content-component.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+// Components
+import { TodoItem } from '../todo-item/todo-item.component'
 
 // Styles
 import './content-component.scss'
 
 export const ContentComponent = () => {
-  return <div>Content</div>
+  const todoList = useSelector((state) => state.todo.todoList)
+
+  const sortedTodos = [...todoList]
+  sortedTodos.sort((a, b) => new Date(b.time) - new Date(a.time))
+
+  return (
+    <div>
+      {sortedTodos && sortedTodos.length > 0
+        ? sortedTodos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+        : 'Todo not found'}
+    </div>
+  )
 }
