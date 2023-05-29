fc2d76b895fef109ce39f226c7f78370340c70a8
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2023-05-29 11:45:07 -0300

    feat: add CheckBoxComponent and it's styles and also add the functionality to change task status and update it state

--- src/components/todo-item/todo-item.component.jsx
+++ src/components/todo-item/todo-item.component.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MdDelete, MdEdit } from 'react-icons/md'
 import { useDispatch } from 'react-redux'
 import { toast } from 'react-hot-toast'
 
+// Components
+import { TodoModalComponent } from '../todo-modal-component/todo-modal.component'
+import { CheckBoxComponent } from '../../check-box-component/check-box.component'
+
 // Reducers & Slices
-import { deleteTodo } from '../../redux/slices/todoSlice'
+import { deleteTodo, updateTodo } from '../../redux/slices/todoSlice'
 
 // Styles
 import './todo-item.styles.scss'
-import { TodoModalComponent } from '../todo-modal-component/todo-modal.component'
 
 export const TodoItem = ({ todo }) => {
-  const dispatch = useDispatch()
-
   const [updateModalOpen, setUpdateModalOpen] = useState(false)
+  const [checked, setChecked] = useState(false)
+
+  const dispatch = useDispatch()
 
   const handleDeleteTodo = () => {
     dispatch(deleteTodo(todo.id))
@@ -24,11 +28,30 @@ export const TodoItem = ({ todo }) => {
     setUpdateModalOpen(true)
   }
 
+  const handleChecked = () => {
+    setChecked(!checked)
+
+    dispatch(
+      updateTodo({
+        ...todo,
+        status: checked ? 'incomplete' : 'complete',
+      })
+    )
+  }
+
+  useEffect(() => {
+    if (todo.status === 'complete') {
+      setChecked(true)
+    } else {
+      setChecked(false)
+    }
+  }, [todo.status])
+
   return (
     <>
       <div className='todo-item'>
         <div className='todo-details'>
-          [ ]
+          <CheckBoxComponent checked={checked} handleChecked={handleChecked} />
           <div className='todo-text'>
             <p
               className={
