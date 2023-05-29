35528a9a6174fec7fe5f2dcf159a76b5876f1262
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2023-05-29 12:25:10 -0300

    feat: add animations from framer-motion and text styles

--- src/components/content-component/content-component.jsx
+++ src/components/content-component/content-component.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { AnimatePresence, motion } from 'framer-motion'
 
 // Components
 import { TodoItem } from '../todo-item/todo-item.component'
 
 // Styles
 import './content-component.scss'
+
+const containerVariant = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+const childVariant = {
+  hidden: { y: 20, opactiy: 0 },
+  visible: {
+    y: 0,
+    opactiy: 1,
+  },
+}
 
 export const ContentComponent = () => {
   const todoList = useSelector((state) => state.todo.todoList)
@@ -24,10 +44,21 @@ export const ContentComponent = () => {
   })
 
   return (
-    <div className='content__wrapper'>
-      {filteredTodoList && filteredTodoList.length > 0
-        ? filteredTodoList.map((todo) => <TodoItem key={todo.id} todo={todo} />)
-        : 'Todo not found'}
-    </div>
+    <motion.div
+      className='content__wrapper'
+      variants={containerVariant}
+      initial='hidden'
+      animate='visible'
+    >
+      <AnimatePresence>
+        {filteredTodoList && filteredTodoList.length > 0 ? (
+          filteredTodoList.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+        ) : (
+          <motion.p className='empty-text' variants={childVariant}>
+            No Todo found
+          </motion.p>
+        )}
+      </AnimatePresence>
+    </motion.div>
   )
 }
