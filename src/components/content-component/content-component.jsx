6494c92342248c29f9f10533a73b011501dccabe
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2023-05-29 12:02:43 -0300

    feat: add filterStatus to the ContentComponent to show correct values of filtered todos

--- src/components/content-component/content-component.jsx
+++ src/components/content-component/content-component.jsx
@@ -10,13 +10,23 @@ import './content-component.scss'
 export const ContentComponent = () => {
   const todoList = useSelector((state) => state.todo.todoList)
 
+  const filterStatus = useSelector((state) => state.todo.filterStatus)
+
   const sortedTodos = [...todoList]
   sortedTodos.sort((a, b) => new Date(b.time) - new Date(a.time))
 
+  const filteredTodoList = sortedTodos.filter((item) => {
+    if (filterStatus === 'all') {
+      return true
+    }
+
+    return item.status === filterStatus
+  })
+
   return (
     <div className='content__wrapper'>
-      {sortedTodos && sortedTodos.length > 0
-        ? sortedTodos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+      {filteredTodoList && filteredTodoList.length > 0
+        ? filteredTodoList.map((todo) => <TodoItem key={todo.id} todo={todo} />)
         : 'Todo not found'}
     </div>
   )
