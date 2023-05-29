ba061336ec62d2a20a71c128d417434c297a00a5
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2023-05-29 11:57:42 -0300

    feat: add filterStatus to the HeaderComponent to show the state of tasks

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from 'react'
-
-// Styles
-import './header.styles.scss'
+import { useDispatch, useSelector } from 'react-redux'
 
 // Components
 import { ButtonComponent, Selector } from '../button-component/button.component'
 import { TodoModalComponent } from '../todo-modal-component/todo-modal.component'
 
+// Reducers & Slices
+import { updateFilterStatus } from '../../redux/slices/todoSlice'
+
+// Styles
+import './header.styles.scss'
+
 export const AppHeaderComponent = () => {
   const [modalOpen, setModalOpen] = useState(false)
+
+  const filterStatus = useSelector((state) => state.todo.filterStatus)
+
+  const dispatch = useDispatch()
+
+  const updateFilter = (e) => {
+    dispatch(updateFilterStatus(e.target.value))
+  }
 
   return (
     <div className='app-header'>
       <ButtonComponent variant='primary' onClick={() => setModalOpen(true)}>
         Add Task
       </ButtonComponent>
-      <Selector id='status'>
+      <Selector id='status' value={filterStatus} onChange={updateFilter}>
         <option value='all'>All</option>
         <option value='incomplete'>Incomplete</option>
         <option value='complete'>Complete</option>
